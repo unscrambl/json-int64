d33e840e7f867b72a15d53561ebeb19065810daf
Mustafa Senol Cosar <mustafa@unscrambl.com>
2018-12-14 16:31:51 +0300

    add tests for int64_util and improve code style

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ module.exports = function(options) {
     return  {
         parse: json_parse(options),
         stringify: json_stringify,
-        util: util
+        util: util,
+        fromDecimalString: util.fromDecimalString,
+        toDecimalString: util.toDecimalString
     }
 };
 //create the default method members with no options applied for backwards compatibility
 module.exports.parse = json_parse();
 module.exports.stringify = json_stringify;
 module.exports.util = util;
+module.exports.fromDecimalString = util.fromDecimalString;
+module.exports.toDecimalString = util.toDecimalString;
--- src/int64_util.js
+++ src/int64_util.js
@@ -17,6 +17,10 @@
  * under the License.
  */
 
+// This file is copied from the Thrift project under Apache 2.0 license
+// There are some changes to variable names and styling
+// The other changes are documented before the changes
+
 var Int64 = require('node-int64');
 
 var Int64Util = module.exports = {};
@@ -26,66 +30,92 @@ var POW2_31 = Math.pow(2, 31);
 var POW2_32 = Math.pow(2, 32);
 var POW10_11 = Math.pow(10, 11);
 
-Int64Util.toDecimalString = function(i64) {
-  var b = i64.buffer;
-  var o = i64.offset;
-  if ((!b[o] && !(b[o + 1] & 0xe0)) ||
-      (!~b[o] && !~(b[o + 1] & 0xe0))) {
-    // The magnitude is small enough.
-    return i64.toString();
-  } else {
-    var negative = b[o] & 0x80;
-    if (negative) {
-      // 2's complement
-      var incremented = false;
-      var buffer = new Buffer(8);
-      for (var i = 7; i >= 0; --i) {
-        buffer[i] = (~b[o + i] + (incremented ? 0 : 1)) & 0xff;
-        incremented |= b[o + i];
-      }
-      b = buffer;
+Int64Util.toDecimalString = function (i64)
+{
+    var i64Buffer = i64.buffer;
+    var i64Offset = i64.offset;
+    if ((!i64Buffer[i64Offset] && !(i64Buffer[i64Offset + 1] & 0xe0)) ||
+        (!~i64Buffer[i64Offset] && !~(i64Buffer[i64Offset + 1] & 0xe0)))
+    {
+        // The magnitude is small enough.
+        return i64.toString();
     }
-    var high2 = b[o + 1] + (b[o] << 8);
-    // Lesser 11 digits with exceeding values but is under 53 bits capacity.
-    var low = b[o + 7] + (b[o + 6] << 8) + (b[o + 5] << 16)
-        + b[o + 4] * POW2_24  // Bit shift renders 32th bit as sign, so use multiplication
-        + (b[o + 3] + (b[o + 2] << 8)) * POW2_32 + high2 * 74976710656;  // The literal is 2^48 % 10^11
-    // 12th digit and greater.
-    var high = Math.floor(low / POW10_11) + high2 * 2814;  // The literal is 2^48 / 10^11
-    // Make it exactly 11 with leading zeros.
-    low = ('00000000000' + String(low % POW10_11)).slice(-11);
-    return (negative ? '-' : '') + String(high) + low;
-  }
+    else
+    {
+        var negative = i64Buffer[i64Offset] & 0x80;
+        if (negative)
+        {
+            // 2's complement
+            var incremented = false;
+            var buffer = new Buffer(8);
+            for (var i = 7; i >= 0; --i)
+            {
+                buffer[i] = (~i64Buffer[i64Offset + i] + (incremented ? 0 : 1)) & 0xff;
+                incremented |= i64Buffer[i64Offset + i];
+            }
+            i64Buffer = buffer;
+        }
+        var high2 = i64Buffer[i64Offset + 1] + (i64Buffer[i64Offset] << 8);
+        // Lesser 11 digits with exceeding values but is under 53 bits capacity.
+        var low = i64Buffer[i64Offset + 7] + (i64Buffer[i64Offset + 6] << 8) + (i64Buffer[i64Offset + 5] << 16) +
+            i64Buffer[i64Offset + 4] * POW2_24 // Bit shift renders 32th bit as sign, so use multiplication
+            +
+            (i64Buffer[i64Offset + 3] + (i64Buffer[i64Offset + 2] << 8)) * POW2_32 + high2 * 74976710656; // The literal is 2^48 % 10^11
+        // 12th digit and greater.
+        var high = Math.floor(low / POW10_11) + high2 * 2814; // The literal is 2^48 / 10^11
+        // The following if block was added when this file was copied from the Thrift project to fix an issue where 
+        // small negative numbers would have unnecessary leading zeros
+        if (!high)
+        {
+            low = String(low % POW10_11).slice(-11);
+            return (negative ? '-' : '') + low;
+        }
+        // Make it exactly 11 with leading zeros.
+        low = ('00000000000' + String(low % POW10_11)).slice(-11);
+        return (negative ? '-' : '') + String(high) + low;
+    }
 };
 
-Int64Util.fromDecimalString = function(text) {
-  var negative = text.charAt(0) === '-';
-  if (text.length < (negative ? 17 : 16)) {
-    // The magnitude is smaller than 2^53.
-    return new Int64(+text);
-  } else if (text.length > (negative ? 20 : 19)) {
-    throw new RangeError('Too many digits for Int64: ' + text);
-  } else {
-    // Most significant (up to 5) digits
-    var high5 = +text.slice(negative ? 1 : 0, -15);
-    var low = +text.slice(-15) + high5 * 2764472320;  // The literal is 10^15 % 2^32
-    var high = Math.floor(low / POW2_32) + high5 * 232830;  // The literal is 10^15 / 2^&32
-    low = low % POW2_32;
-    if (high >= POW2_31 &&
-        !(negative && high == POW2_31 && low == 0)  // Allow minimum Int64
-       ) {
-      throw new RangeError('The magnitude is too large for Int64.');
+Int64Util.fromDecimalString = function (text)
+{
+    var negative = text.charAt(0) === '-';
+    if (text.length < (negative ? 17 : 16))
+    {
+        // The magnitude is smaller than 2^53.
+        return new Int64(+text);
     }
-    if (negative) {
-      // 2's complement
-      high = ~high;
-      if (low === 0) {
-        high = (high + 1) & 0xffffffff;
-      } else {
-        low = ~low + 1;
-      }
-      high = 0x80000000 | high;
+    else if (text.length > (negative ? 20 : 19))
+    {
+        throw new RangeError('Too many digits for Int64: ' + text);
     }
-    return new Int64(high, low);
-  }
+    else
+    {
+        // Most significant (up to 5) digits
+        var high5 = +text.slice(negative ? 1 : 0, -15);
+        var low = +text.slice(-15) + high5 * 2764472320; // The literal is 10^15 % 2^32
+        var high = Math.floor(low / POW2_32) + high5 * 232830; // The literal is 10^15 / 2^&32
+        low = low % POW2_32;
+        // The equality operators were changed to '===' from '==' when this file was copied from the Thrift project
+        if (high >= POW2_31 &&
+            !(negative && high === POW2_31 && low === 0) // Allow minimum Int64
+        )
+        {
+            throw new RangeError('The magnitude is too large for Int64.');
+        }
+        if (negative)
+        {
+            // 2's complement
+            high = ~high;
+            if (low === 0)
+            {
+                high = (high + 1) & 0xffffffff;
+            }
+            else
+            {
+                low = ~low + 1;
+            }
+            high = 0x80000000 | high;
+        }
+        return new Int64(high, low);
+    }
 };
